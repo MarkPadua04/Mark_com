dc19d96d3385ef8c31128f91f2f7680bcce499b8
MarkPadua04 <90506557+MarkPadua04@users.noreply.github.com>
2021-11-15 10:15:41 +0800

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,21 @@
+  $('#myTab a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  })
+
+
+
+/*  This sections var JS About Me ! */
+  var name = "Mark Francis Padua";
+  var future = "IT Expert";
+
+  document.write('<p class= "name">'  +  name  + '</p>')
+  document.write('<p class= "future">'  + future + '</p>')
+
+
+
+
+
+
+
+
